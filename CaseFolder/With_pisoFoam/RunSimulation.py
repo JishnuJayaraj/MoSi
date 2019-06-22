131da70523c8d8053e1f6f83360f82f9eccda76c
ajucgnr <ajucgnr@gmail.com>
2019-06-22 10:51:48 +0200

    deltaT changed to 0.0005 and simulation o/p prints to txt

--- CaseFolder/With_pisoFoam/RunSimulation.py
+++ CaseFolder/With_pisoFoam/RunSimulation.py
@@ -25,9 +25,10 @@ for line in transport_property:
 		break
 nu = float(splitline[8])
 
-for i in range(2):
+for i in range(100):
 	if Path(Itpath+str(i+1)).exists() == False:
 		os.mkdir('Iteration_'+str(i+1))
+	print('Starting Iteration '+str(i+1)+'.....')
 	os.system('cp -r '+Refpath+'/* '+Itpath+str(i+1))
 	os.chdir(Itpath+str(i+1)+'/NS')
 
@@ -124,42 +125,52 @@ for i in range(2):
 						p = 0
 						end = 10
 					flagSpline = False
-
-		os.system('blockMesh')
-		os.system('checkMesh')
+		
+		os.system('blockMesh >outputblockMesh.txt')
+		os.system('checkMesh >outputcheckMesh.txt')
+		
 		#ToDo: if checkmesh last but second line says mesh: ok, then execute next line
-		os.system('pisoFoam')
+		print('Iter-'+str(i+1)+' (NS): Meshing done and checked!')
+		print('Iter-'+str(i+1)+' (NS): Running solver...')
+		os.system('pisoFoam > outputNS.txt')
+		print('Iter-'+str(i+1)+' (NS): End of solution!')
 	else:
 		print('Block Mesh Dictionary is not available')
 		sys.exit(1)
 
 	os.system('cp '+Itpath+str(i+1)+'/NS/1.5/p '+Itpath+str(i+1)+'/OS/0')
 	os.system('cp '+Itpath+str(i+1)+'/NS/1.5/U '+Itpath+str(i+1)+'/OS/0')
-
+	
 	dragForceFile   = open(Itpath+str(i+1)+'/NS/postProcessing/forces/0/forces.dat','r')
 	lastline        = dragForceFile.readlines()
 	lastline        = lastline[-1]
 	term = '[^(())\t\n ]+'
 	splitDoubleBraces = re.findall(term, lastline)
 	DragForceX      = float(splitDoubleBraces[1])+float(splitDoubleBraces[4])
-	
+	print('Iter-'+str(i+1)+': Writing drag force to file..!')
 	forceResultfile.write(str(i)+"\t"+str(DragForceX)+"\n")
 
 	f = fileinput.FileInput(Itpath+str(i+1)+'/OS/0/UAdj', inplace=True)
 	for line in f:
-		findText = 'value           uniform ('+str(DragInitialX)+' 0 0);'
+		findText = 'value           uniform (1 0 0);'
 		replaceText = 'value           uniform ('+str(DragForceX)+' 0 0);'
 		print(line.replace(findText, replaceText), end='')
-	DragInitialX = DragForceX
+	
 	f.close()
 
 	os.chdir(Itpath+str(i+1)+'/OS')
 	os.system('cp '+Itpath+str(i+1)+'/NS/system/blockMeshDict '+Itpath+str(i+1)+'/OS/system/')
-	os.system('blockMesh')
-	os.system('checkMesh')
-	os.system('pisomosiFoam')
+	os.system('blockMesh >outputblockMesh.txt')
+	os.system('checkMesh >outputcheckMesh.txt')
+	
+	
+	print('Iter-'+str(i+1)+' (OS): Meshing done and checked!')
+	print('Iter-'+str(i+1)+' (OS): Running solver...')
+	os.system('pisomosiFoam >outputOS.txt')
+	print('Iter-'+str(i+1)+' (OS): End of solution!')
 
 	# Calculation of new boundary Coordinates
+	print('Iter-'+str(i+1)+': Calculating new boundary points...')
 	#TODO=====> DONE <======: to be read from the Transportproperties dictfile
 	#nu = .01 # viscosity =====> Added above : read only once from Reference
 
@@ -246,15 +257,18 @@ for i in range(2):
 
 		global_Corrector=np.append(global_Corrector,corrector)
 		global_pos = np.append(global_pos,pos)
-	
-	while weight > 1e-8:
+	minWeight = 1e-8
+	while weight > minWeight:
 		updated_global_pos = global_pos - weight*global_Corrector
 		withinTolFlag = np.allclose(updated_global_pos,global_pos,rtol=.1, atol=1e-08, equal_nan=False)
 		if not withinTolFlag:
 			weight = 0.1*weight
 		else:
 			break
-	print(weight)
+	if weight<minWeight:
+		print('Weight used: '+str(weight/0.1))
+	else:
+		print('Weight used: '+str(weight))
 	UnitConversionFactor = 0.1 #TODO to be read from blockMeshDict file
 
 	updated_global_pos = updated_global_pos*1/UnitConversionFactor
@@ -334,5 +348,6 @@ for i in range(2):
 	boundaryVertices = np.vstack([boundaryVertices,boundaryVertices+zOffsetMatrix])
 
 	os.chdir(Modpath)
-	
+	print('End of Iteration '+str(i+1)+'.')
+	print('\n')
 forceResultfile.close()
